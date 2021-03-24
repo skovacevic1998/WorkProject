2c30e8d2da41cf1d748c23907372400eef73442e
Siniša Kovačević <skovacevic@vub.hr>
2021-03-24 12:58:24 +0100

    Dizajn Profil.fxml nadodan, dodjeljivanje id komponentama Register.fxml i Profil.fxml, prosirenje Users.java klase.

--- src/backend/Users.java
+++ src/backend/Users.java
@@ -9,15 +9,34 @@ public class Users extends Controller {
 	String username;
 	String password;
 	String email;
+	String ime;
+	String prezime;
+	String drzava;
+	String grad;
+	String brojMobitela;
+	int pozicija;
+	float placaProslogMjeseca;
+	float putniTroskovi;
+	float bodovi;
+	float satiMjesecno;
+	float ukupnoStecenihRadnihSati;
+	int preostaliDaniGodisnjegOdmora;
 
-	public Users(String username, String password, String email) {
+	public Users(String username, String password, String email, String ime, String prezime, String drzava, String grad, String brojMobitela, int pozicija, float placaProslogMjeseca, float putniTroskovi, float bodovi, float satiMjesecno, float ukupnoStecenihRadnihSati, int preostaliDaniGodisnjegOdmora) {
 		this.username = username;
 		this.password = password;
 		this.email = email;
+		this.ime = ime;
+		this.prezime = prezime;
+		this.drzava = drzava;
+		this.grad = grad;
+		this.brojMobitela = brojMobitela;
+		this.pozicija = pozicija;
+		this.placaProslogMjeseca = placaProslogMjeseca;
+		this.putniTroskovi = putniTroskovi;
+		this.bodovi = bodovi;
+		this.satiMjesecno = satiMjesecno;
+		this.ukupnoStecenihRadnihSati = ukupnoStecenihRadnihSati;
+		this.preostaliDaniGodisnjegOdmora = preostaliDaniGodisnjegOdmora;
 	}
-
-	public void registrationHandler() {
-
-	}
-
 }
